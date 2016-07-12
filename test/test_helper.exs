32886edebe398d0e0d06d6331d3d04cebd48b336
dania02525 <dania02525@gmail.com>
2016-07-12 14:37:49 -0500

    config for ci test

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,4 @@
+Application.put_env(:easypost, :easypost_endpoint, System.get_env("EASYPOST_ENDPOINT"))
+Application.put_env(:easypost, :easypost_test_key, System.get_env("EASYPOST_TEST_KEY"))
+
 ExUnit.start()
